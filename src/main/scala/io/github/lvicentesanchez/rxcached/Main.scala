4956a5ad44e30c3f0fe18a70cc3dbfb8fd399405
Luis Angel Vicente Sanchez <langel.vicente@gmail.com>
2015-02-22 23:01:12 +0000

    * use one ByteString per command.

--- src/main/scala/io/github/lvicentesanchez/rxcached/Main.scala
+++ src/main/scala/io/github/lvicentesanchez/rxcached/Main.scala
@@ -1,11 +1,10 @@
 package io.github.lvicentesanchez.rxcached
 
-import java.net.InetSocketAddress
-
 import akka.actor.ActorSystem
 import akka.stream.{ ActorFlowMaterializer, FlowMaterializer }
 import akka.stream.scaladsl._
 import akka.util.ByteString
+import java.net.InetSocketAddress
 import scala.concurrent.ExecutionContext
 
 object Main extends App {
@@ -16,7 +15,11 @@ object Main extends App {
 
   val connection: StreamTcp.OutgoingConnection = StreamTcp().outgoingConnection(new InetSocketAddress("localhost", 11211))
 
-  Source.single(ByteString("add key1 0 0 6\r\nvalue4\r\nget key1\r\nstats items\r\n")).
+  Source(List(
+    ByteString("add key1 0 0 6\r\nvalue4\r\n"),
+    ByteString("get key1\r\n"),
+    ByteString("stats items\r\n")
+  )).
     via(connection.flow).
     runWith(Sink.foreach(str => println(str.decodeString("UTF-8")))).
     onComplete {
